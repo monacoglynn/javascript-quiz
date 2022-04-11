ec84cb4e5d77f31c0b2cc5f7865fb4c425f6089e
monacoglynn <96494895+monacoglynn@users.noreply.github.com>
2022-04-11 11:22:11 -0700

    made the html splash page and started with some styles. Added the timer function and began working on the start quiz function.

---
+++ assets/js/scripts.js
@@ -0,0 +1,32 @@
+var startButton = document.querySelector('.start-button');
+var timer = document.querySelector('.timer');
+var title = document.querySelector('.splash');
+var welcome = document.querySelector('.welcome');
+
+
+var timeLeft;
+
+
+function countdown() {
+    timeLeft = 75;
+    var timeInterval = setInterval(function () {
+        timeLeft--;
+        if (timeLeft > 0) {
+            timer.textContent = timeLeft;
+        } else {
+            timer.textContent = 0;
+            console.log('you lose');
+            clearInterval(timeInterval);
+        }
+    }, 1000);
+}
+
+function startQuiz() {
+    timer.textContent = 75;
+    countdown();
+    startButton.setAttribute('style', 'display: none;');
+    title.setAttribute('style', 'display: none;');
+}
+
+
+startButton.addEventListener('click', startQuiz);
