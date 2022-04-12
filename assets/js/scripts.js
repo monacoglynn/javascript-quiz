3bd5380b6458508d50acbac54cd0fe54b1386e73
monacoglynn <96494895+monacoglynn@users.noreply.github.com>
2022-04-12 14:17:16 -0700

    worked with a tutor and found a better data structure for my questions. added some conditions depending on the choice, and started building the highscores pages

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,78 +1,123 @@
 var startButton = document.querySelector('.start-button');
 var timer = document.querySelector('.timer');
 var title = document.querySelector('.splash');
-var welcome = document.querySelector('.welcome');
+var infoText = document.querySelector('.info-text');
+var questionDiv = document.querySelector('.quiz-box');
 var main = document.body.children[1];
-var h2El = document.createElement('h2');
-var btn1 = document.createElement('button');
-var btn2 = document.createElement('button');
-var btn3 = document.createElement('button');
-var btn4 = document.createElement('button');
-var questionDiv = document.createElement('div');
-
-var question1 = {
-    question: 'How many keys does a piano have?',
-    ans1: '80 keys',
-    ans2: '88 keys',
-    ans3: '100 keys',
-    ans4: '75 keys'
-}
-
-var question2 = {
-    question: 'What is the full name of the piano?',
-    ans1: 'Pianissimo',
-    ans2: 'Fortepiano',
-    ans3: 'Pianoforte',
-    ans4: 'Trick question, the full name is piano',
-}
+var newH2 = document.createElement('h2');
 
 
+
+var listOfQs = [{
+        question: 'How many keys does a piano have?',
+        choices: ['80 keys', '88 keys', '100 keys', '75 keys'],
+        answer: '88 keys'
+    },
+    {
+        question: 'What is the full name of the piano?',
+        choices: ['Pianissimo', 'Fortepiano', 'Pianoforte', 'Trick question, the full name is piano'],
+        answer: 'Pianoforte'
+    },
+    {
+        question: 'What time period was the piano invented?',
+        choices: ['1500s', '1600s', '1700s', '1800s', ],
+        answer: '1700s'
+    },
+    {
+        question: ''
+    }
+]
+var currentQuestion;
+var highScore = [];
 var timeLeft;
 
 //get stats from local storage
+function init() {
+    var storedHighScore = JSON.parse(localStorage.getItem('highScore'))
+    // localStorage.setItem("saveHighscore", JSON.stringify(array))
+    if (storedHighScore !== null) {
+        highScore = storedHighScore;
+    }
+}
+
+init();
+
+
 
 function countdown() {
     timeLeft = 75;
     var timeInterval = setInterval(function () {
         timeLeft--;
-        if (timeLeft > 0) {
+        if (timeLeft > 0 && currentQuestion < listOfQs.length) {
             timer.textContent = timeLeft;
             // if wrong answer
             // if you win stop timer
-        } else {
+        } else if (timeLeft = 0) {
             timer.textContent = 0;
             console.log('you lose');
             clearInterval(timeInterval);
+        } else {
+            console.log('you win!')
+            clearInterval(timeInterval);
+
         }
     }, 1000);
 }
 
-function question(quiz) {
-    main.appendChild(h2El);
-    main.appendChild(questionDiv);
-    main.children[4].appendChild(btn1);
-    main.children[4].appendChild(btn2);
-    main.children[4].appendChild(btn3);
-    main.children[4].appendChild(btn3);
-    main.children[4].appendChild(btn4);
+//write function to display high scores after quiz is finished.
+// function showHighScore() {}
 
-    h2El.textContent = quiz.question;
-    btn1.textContent = quiz.ans1;
-    btn2.textContent = quiz.ans2;
-    btn3.textContent = quiz.ans3;
-    btn4.textContent = quiz.ans4;
+function enterNameHighScore() {
+    questionDiv.innerHTML = '';
+    newH2.textContent = 'Please enter your initials';
+    questionDiv.appendChild(newH2);
+    var highScoreForm = document.createElement('input');
+    questionDiv.appendChild(highScoreForm);
+    var newBtn = document.createElement('button')
+    newBtn.textContent = 'Submit'
+    newBtn.addEventListener('click', showHighScore)
+    questionDiv.appendChild(newBtn);
 
-    // create event listener for buttons.
-    //create right answer button.
 }
 
-function nextQuestion(quiz) {
-    h2El.textContent = quiz.question;
-    btn1.textContent = quiz.ans1;
-    btn2.textContent = quiz.ans2;
-    btn3.textContent = quiz.ans3;
-    btn4.textContent = quiz.ans4;
+function resolveAnswer(event) {
 
+    var userPick = event.target.textContent
+    console.log(userPick)
+    // console.log(this)
+
+    if (userPick == listOfQs[currentQuestion].answer) {
+        //nice well done
+        console.log("correct")
+        timeLeft += 4;
+    } else {
+        //decrement time remaining
+        console.log("incorrect")
+        timeLeft -= 5;
+    }
+    currentQuestion++;
+    if (currentQuestion < listOfQs.length) {
+        showQuestion()
+    } // show high score entry screen
+    else {
+        enterNameHighScore();
+    }
+
+}
+
+function showQuestion() {
+    // questionDiv
+    questionDiv.innerHTML = '';
+
+    newH2.textContent = listOfQs[currentQuestion].question;
+    questionDiv.appendChild(newH2);
+
+    for (i = 0; i < listOfQs[currentQuestion].choices.length; i++) {
+        var newBtn = document.createElement("button");
+        newBtn.textContent = listOfQs[currentQuestion].choices[i];
+        newBtn.addEventListener('click', resolveAnswer);
+        questionDiv.appendChild(newBtn)
+    }
 }
 
 
@@ -81,8 +126,9 @@ function startQuiz() {
     countdown();
     startButton.setAttribute('style', 'display: none;');
     title.setAttribute('style', 'display: none;');
-    welcome.setAttribute('style', 'display: none;');
-    question(question1);
+    infoText.setAttribute('style', 'display: none;');
+    currentQuestion = 0;
+    showQuestion();
     // remove last questions
     // add next question.
     //right answer
